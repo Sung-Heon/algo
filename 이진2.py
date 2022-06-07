a06c5ead75ea585df0844ef593bbdc93438b144d
Sungheon Jeong <elon4856@gmail.com>
2022-06-07 20:19:29 +0900

    Create 이진2.py

---
+++ 이진2.py
@@ -0,0 +1,16 @@
+#리트코드35
+#왜 내답은 틀렸었을까?
+# 이렇게 하는게 편할지도
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            pivot = (left + right) // 2
+            if nums[pivot] == target:
+                return pivot
+            if target < nums[pivot]:
+                right = pivot - 1
+            else:
+                left = pivot + 1
+        return left
